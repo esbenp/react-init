0e5ecc0fe732f5a13b0f7035e1c174c180a1225c
Esben Petersen <esbenspetersen@gmail.com>
2017-09-05 18:32:56 +0200

    Remove unused button

--- src/app/Dashboard/index.js
+++ src/app/Dashboard/index.js
@@ -8,7 +8,6 @@ class Dashboard extends Component<void, void, void> {
     return (
       <div>
         Dashboard
-        <button type="button" onClick={() => this.test()}>Test</button>
       </div>
     )
   }
